63381981273e16a2942b2a7a12f4ccc8ef16001b
Kristian Gutic <485109@ics.muni.cz>
2020-12-10 19:09:38 +0100

    added basic plotting for debug

--- Pipeline/Plotting.py
+++ Pipeline/Plotting.py
@@ -1,8 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Plot:
 
     @staticmethod
-    def plot_band_as_img(band: np.ndarray):
-        pass
+    def plot_image(band: np.ndarray):
+        plt.imshow(band)
+        plt.show()
+
+    @staticmethod
+    def plot_mask(mask):
+        plt.imshow(mask, cmap='binary')
+        plt.show()
