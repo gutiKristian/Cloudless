c808ffad8bc57bb3174a1325c0a64c5b47e6856d
Kristián Gutič <485109@muni.cz>
2020-12-31 23:07:03 +0100

    description added

--- Pipeline/PipelineBand.py
+++ Pipeline/PipelineBand.py
@@ -2,6 +2,7 @@ import numpy as np
 import gc
 from osgeo import gdal
 from Pipeline.utils import *
+import numpy as np
 
 gdal.UseExceptions()
 
@@ -21,9 +22,13 @@ class Band:
         if load_on_init:
             self.load_raster()
 
-    def init_gdal(self) -> gdal.Dataset:
+    def init_gdal(self) -> None:
+        """
+        This is method initializes the raster image (opens it with gdal and grabs the meta-data).
+        :return: None
+        """
         if self.is_opened:
-            return self._gdal
+            return
         try:
             self._gdal = gdal.Open(self.path)
             self.projection = self._gdal.GetProjection()
@@ -31,9 +36,12 @@ class Band:
             self.is_opened = True
         except Exception as e:
             raise Exception("GDAL thrown an error: ", e)
-        return self._gdal
 
     def load_raster(self) -> None:
+        """
+        Since the raster images might be quite big,
+        we do not want them casually lay in our memory therefore we can choose when we want them.
+        """
         if self._was_raster_read:
             return
         if not self.is_opened:
@@ -44,12 +52,18 @@ class Band:
             slice_raster(self.slice_index, self.raster_image)
 
     def raster(self) -> np.array:
+        """
+        Basic getter.
+        """
         if self._was_raster_read:
             return self.raster_image
         self.load_raster()
         return self.raster_image
 
     def free_resources(self) -> None:
+        """
+        Delete the pointers to the data and call garbage collector to free the memory.
+        """
         del self._gdal
         del self.raster_image
         self._was_raster_read = False
@@ -57,9 +71,13 @@ class Band:
         gc.collect()  # Garbage collector
 
     def __gt__(self, other: int):
-        #  call raster method in case raster image has not been initialized
+        """
+        :param other: threshold value
+        """
         return np.copy(self.raster()) > other
 
     def __lt__(self, other: int):
-        #  call raster method in case raster image has not been initialized
+        """
+        :param other: threshold value
+        """
         return np.copy(self.raster()) < other
