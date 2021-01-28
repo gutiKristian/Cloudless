26fc4911b69e3c6994c343a6de929b6aeb194d49
Kristian Gutic <485109@gitlab.ics.muni.cz>
2021-01-28 15:56:34 +0100

    added method for s2 ndvi optimization

--- Pipeline/Masks.py
+++ Pipeline/Masks.py
@@ -1,11 +1,33 @@
 """
 Faster calculations with numba
 """
-from numba import njit
+from numba import njit, prange
 import math
+import numpy as np
 
 
 class S2JIT:
+
+    @staticmethod
+    @njit(parallel=True)
+    def s2_ndvi_pixel_masking(ndvi, offset=0):
+        """
+        Per pixel masking of the best ndvi pixels.
+        @param ndvi: ndvi of each worker
+        @param offset: offsetting workers, making ndvi with workers[10:20] -> offset = 9
+        :return: mask of indices of the workers and worker map of used workers
+        """
+        result = np.zeros(shape=(ndvi.shape[1], ndvi.shape[2]))
+        workers = np.zeros(ndvi.shape[0] + offset)
+        x_res = ndvi.shape[1]
+        y_res = ndvi.shape[2]
+        for y in prange(y_res):
+            for x in prange(x_res):
+                r = ndvi[:, x, y].argmax() + offset  # if we had to split this operation
+                if workers[r] == 0:
+                    workers[r] = 1
+                result[x, y] = r
+        return result, workers
 
     @staticmethod
     @njit
