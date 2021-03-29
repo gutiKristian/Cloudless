7b0517c4f5d7eebf5510e21a8d073d555c0fe0e0
Kristian Gutic <485109@gitlab.ics.muni.cz>
2021-03-29 16:47:48 +0200

    rgb enhc.

--- Pipeline/Worker.py
+++ Pipeline/Worker.py
@@ -73,7 +73,7 @@ class S2Worker:
         profile = list(self.granules[-1].bands[self.spatial_resolution].values())[0].profile
         for key in self.result.keys():
             path = self.save_result_path + os.path.sep + key + "_" + str(self.spatial_resolution)
-            GranuleCalculator.save_band_rast(self.result[key], path=path, profile=profile, driver="GTiff")
+            GranuleCalculator.save_band_rast(self.result[key], path=path, prof=profile, driver="GTiff")
             # GranuleCalculator.save_band(raster_img=self.result[key], name=key + "_" + str(self.spatial_resolution),
             #                             path=path, projection=projection, geo_transform=geo_transform)
 
