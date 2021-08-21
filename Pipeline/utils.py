eaf51199ac6f78b01a7407c1cdbe4d49ddf5798e
Kristián Gutič <485109@mail.muni.cz>
2021-08-21 19:47:11 +0200

    fix

--- Pipeline/utils.py
+++ Pipeline/utils.py
@@ -319,6 +319,6 @@ def supported_granule_type(_type: str) -> bool:
 
 
 def get_metadata_path(granule_path: str, granule_type: str) -> str:
-    if supported_granule_type(granule_type):
+    if not supported_granule_type(granule_type):
         raise NotImplemented
     return granule_path + os.path.sep + ("MTD_MSIL2A.xml" if granule_type == "L2A" else "MTD_MSIL1C.xml")
